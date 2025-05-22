b05eda1ff861a6639823e29adc5b07324ca9cb43
Aleksander Błaszkiewicz <aleksander.blaszkiewicz@flip.shop>
2025-05-22 16:26:10 +0200

    feat(): wip

--- demo/run.sh
+++ demo/run.sh
@@ -1,16 +1,8 @@
 #!/bin/sh
 set -e
 
-cd "$(dirname "$0")/.."
-echo "Building LogDash demo Docker image (using published gem)..."
 docker build --no-cache -t logdash-demo -f demo/Dockerfile .
 
-echo
-echo "Running LogDash demo..."
-echo
 docker run --rm -it \
   -e LOGDASH_API_KEY="${LOGDASH_API_KEY}" \
   logdash-demo
-
-echo
-echo "Demo completed!" 
